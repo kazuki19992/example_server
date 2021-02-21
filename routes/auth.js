f8a8d35694f010be9af60864a2299c3ec740e2ae
kazuki19992 <kushida98@gmail.com>
2021-02-21 19:25:45 +0900

    htmlファイルを返すように設定

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path')
+
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  console.log(req.body);
   const data = req.body
   if(data.name === "userName" && data.pass === "password"){
     console.log("ログイン成功")
-    res.send("Login success!")
+    res.sendFile(path.resolve('public/success.html'))
   }else{
     console.log("ログイン失敗")
-    res.send("Login failed...")
+    res.sendFile(path.resolve('public/failed.html'))
   }
 });
 
